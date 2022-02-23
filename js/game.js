555dff0f08003d7839ac27be7f0d8956e8531b84
Patrick Coxall <mr.coxall@mths.ca>
2022-02-23 12:00:45 -0500

    add a scene

--- js/game.js
+++ js/game.js
@@ -1,21 +1,25 @@
 /* global Phaser */
 
+import MainScene from "./mainScene.js";
+
 const config = {
   type: Phaser.Auto,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      debug: true
-    }
+      debug: true,
+    },
   },
-  backgroundColor: 0X5f6e7a,
+  backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.Fit,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
-}
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
 
-const game = new Phaser.Game(config)
-console.log(game)
+const game = new Phaser.Game(config);
+
+game.scene.add("mainScene", mainScene);
+game.scene.start("mainScene");
---
+++ js/mainScene.js
@@ -0,0 +1,19 @@
+/* global Phaser */
+
+class MainScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "mainScene" });
+  }
+
+  init(data) {
+    this.cameras.main.setBackgroundColor("ffffff");
+  }
+
+  preload() {
+    console.log("main scene");
+  }
+
+  create(data) {}
+
+  update(time, delta) {}
+}
