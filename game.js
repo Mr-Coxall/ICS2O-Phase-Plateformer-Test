bcd6db6c34b0c63012282918f2c4bc025ba60a12
Patrick Coxall <mr.coxall@mths.ca>
2022-02-23 11:46:40 -0500

    added game object

---
+++ game.js
@@ -0,0 +1,11 @@
+/* global Phaser */
+
+const config = {
+  type: Phaser.Auto,
+  width 800,
+  height: 600,
+  backgroundColor: 0X5f6e7a
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
